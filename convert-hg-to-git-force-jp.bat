b2fe3789ebb0dcafbc766927ab546e6176a386f1
toras9000 <toras9000@example.com>
2023-02-11 20:51:04 +0900

    パラメータ指定のサンプルとしてバッチファイルを置いておく

---
+++ convert-hg-to-git-force-jp.bat
@@ -0,0 +1,16 @@
+@echo off
+
+setlocal 
+
+set script_path=%~dp0
+set convert_script=%script_path%/convert-hg-to-git.csx
+
+echo win32mbcs / ShiftJIS の利用が前提として強制モードで変換実行
+dotnet script "%convert_script%" -- ^
+    --win32mbcs true ^
+    --file-enc cp932 ^
+    --force true ^
+    --copy-authors true
+
+pause
+
